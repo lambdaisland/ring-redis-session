fc1426585a330b9fddec849662f96aab0255efb0
Daniel Große <daniel.grosse@staffbase.com>
2021-11-15 14:30:17 +0100

    test: add unit tests

--- test/ring/redis/session_test.clj
+++ test/ring/redis/session_test.clj
@@ -1,7 +1,48 @@
 (ns ring.redis.session-test
-  (:use [ring.redis.session])
-  (:use [clojure.test]))
+  (:require [clojure.test :refer [deftest is]]
+            [ring.redis.session :as rs]
+            [ring.middleware.session.store :as ring-store]))
 
-;; XXX write some tests ...
-(deftest replace-me
-  (is true))
+(def conn {:pool {}
+           :spec {:host "127.0.0.1"
+                  :port 6379}})
+
+(deftest store-creation
+  (let [store (rs/redis-store conn)]
+    (is (true? (instance? ring.redis.session.RedisStore store)))))
+
+(deftest store-retrieve-session-test
+  (let [store (rs/redis-store conn)]
+    (is (= "key" (ring-store/write-session store "key" {:foo :bar})))
+    (is (= {:foo :bar} (ring-store/read-session store "key")))))
+
+(deftest delete-session-test
+  (let [store (rs/redis-store conn)]
+    (ring-store/write-session store "key" {:foo :bar})
+    (is (= {:foo :bar} (ring-store/read-session store "key")))
+    (is (nil? (ring-store/delete-session store "key")))
+    (is (nil? (ring-store/read-session store "key")))))
+
+(deftest automatic-timeout-test
+  (let [store (rs/redis-store conn {:expire-secs 1})]
+    (ring-store/write-session store "key" {:foo :bar})
+    (Thread/sleep 1100)
+    (is (nil? (ring-store/read-session store "key")))))
+
+(deftest automatic-renewal-test
+  (let [store (rs/redis-store conn {:expire-secs 1 :reset-on-read true})]
+    (ring-store/write-session store "key" {:foo :bar})
+    (Thread/sleep 500)
+    (ring-store/read-session store "key")
+    (Thread/sleep 600)
+    (is (= {:foo :bar} (ring-store/read-session store "key")))))
+
+(deftest custom-read-handler-test
+  (let [store (rs/redis-store conn {:read-handler (fn [_] {:foo :baz})})]
+    (ring-store/write-session store "key" {:foo :bar})
+    (is (= {:foo :baz} (ring-store/read-session store "key")))))
+
+(deftest custom-write-handler-test
+  (let [store (rs/redis-store conn {:read-handler (fn [_] "foo")})]
+    (ring-store/write-session store "key" {:foo :bar})
+    (is (= "foo" (ring-store/read-session store "key")))))
